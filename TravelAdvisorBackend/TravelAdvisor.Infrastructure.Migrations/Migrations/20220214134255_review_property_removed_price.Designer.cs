75c794831cdbfcfe24e16a64f5a2740446255122
Alexander Stenberg <a-stenberg@live.se>
2022-02-14 14:43:24 +0100

    Update - Attraction Propchange
    Removed Prop "Rrice"

--- TravelAdvisorBackend/TravelAdvisor.Infrastructure.Migrations/Migrations/20220214134255_review_property_removed_price.Designer.cs
+++ TravelAdvisorBackend/TravelAdvisor.Infrastructure.Migrations/Migrations/20220214134255_review_property_removed_price.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TravelAdvisor.Infrastructure.Migrations.Data;
 
 namespace TravelAdvisor.Infrastructure.Migrations.Migrations
 {
     [DbContext(typeof(DbApplicationContext))]
-    partial class DbApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20220214134255_review_property_removed_price")]
+    partial class review_property_removed_price
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TravelAdvisorBackend/TravelAdvisor.Infrastructure.Migrations/Migrations/20220214134255_review_property_removed_price.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TravelAdvisor.Infrastructure.Migrations.Migrations
+{
+    public partial class review_property_removed_price : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Attractions");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Price",
+                table: "Attractions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Attractions",
+                keyColumn: "Id",
+                keyValue: new Guid("0f8fad5b-d9cb-469f-a165-70867728950e"),
+                column: "Price",
+                value: 100);
+
+            migrationBuilder.UpdateData(
+                table: "Attractions",
+                keyColumn: "Id",
+                keyValue: new Guid("252e9eea-9f1e-4fe2-bbf2-bcc17d1df943"),
+                column: "Price",
+                value: 100);
+
+            migrationBuilder.UpdateData(
+                table: "Attractions",
+                keyColumn: "Id",
+                keyValue: new Guid("514fae50-43a7-4958-9abd-c549817b9414"),
+                column: "Price",
+                value: 100);
+
+            migrationBuilder.UpdateData(
+                table: "Attractions",
+                keyColumn: "Id",
+                keyValue: new Guid("9b6cd306-a29f-40f5-9ba6-25906164d849"),
+                column: "Price",
+                value: 100);
+
+            migrationBuilder.UpdateData(
+                table: "Attractions",
+                keyColumn: "Id",
+                keyValue: new Guid("e0044d7a-3b3b-4a10-b67c-d85b2181fbe9"),
+                column: "Price",
+                value: 100);
+        }
+    }
+}
